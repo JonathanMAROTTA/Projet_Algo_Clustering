a623dc6eff3a78703c87debed621fc0f18ec87d6
Beaufils Tao - Fix system <beaufilstao@gmail.com>
2023-01-26 18:22:31 +0100

    First commit

--- connectes.py
+++ connectes.py
@@ -35,17 +35,12 @@ def print_components_sizes(distance, points):
     last_efficient_x = 0
     groups, register = {}, {}
 
-    pts2, seg1, seg2, nb_comparaison = [], [], [], 0
-
     for i, point in enumerate(points):
-        nb_comparaison += 1
         if i not in register.keys():
             x, y = point.coordinates
 
             # Search last efficient index
-            print(i, last_efficient_x)
             while last_efficient_x < len(points) and points[last_efficient_x].coordinates[0] <= x - distance:
-                nb_comparaison += 1
                 last_efficient_x += 1
 
             # Create new group
@@ -54,12 +49,9 @@ def print_components_sizes(distance, points):
 
             j = last_efficient_x
             while j < len(points) and points[j].coordinates[0] <= x + distance:
-                nb_comparaison += 1
                 aside_x, aside_y = points[j].coordinates
 
-                nb_comparaison += 1
                 if y - distance <= aside_y <= y + distance and point.distance_to(points[j]) <= distance:
-                    nb_comparaison += 1
                     if j in register.keys() and i not in groups[register[j]]:
                         aside_group_id = register[j]
                         aside_group = groups[aside_group_id]
@@ -76,23 +68,7 @@ def print_components_sizes(distance, points):
                     register[j] = register[i]
                     groups[register[i]].add(j)
 
-                    seg1.append(Segment([point, points[j]]))
-    
-                seg2.append(Segment([point, points[j]]))
                 j += 1
-
-            pts2.append(point)
-            cercle = [Point([distance * cos(c*pi/10), distance * sin(c*pi/10)]) + point for c in range(20)]
-            seg2.append((Segment([p1, p2]) for p1, p2 in zip(cercle, islice(cycle(cercle), 1, None))))
-
-
-
-    # Display
-    print('Comparaison de point :', nb_comparaison)
-    tycat(pts2, *seg2)
-
-    # Display
-    tycat(points, *seg1)
 
     result = list(len(group) for group in groups.values())
     result.sort(reverse=True)
