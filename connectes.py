9daccbc1e2061c17ba8fcbaef415ebaaee838172
Tao Beaufils <tao.beaufils@ensimag.fr>
2023-01-27 17:56:26 +0100

    Last efficient removes

--- connectes.py
+++ connectes.py
@@ -8,6 +8,7 @@ from math import cos, sin, pi
 from timeit import timeit
 from sys import argv
 from itertools import islice, cycle
+from time import perf_counter
 
 from geo.tycat import tycat
 from geo.point import Point
@@ -30,74 +31,68 @@ def print_components_sizes(distance, points):
     """
     affichage des tailles triees de chaque composante
     """
+    start = perf_counter()
+
     points.sort()
 
-    last_efficient_x = 0
-    groups, register = {}, {}
+    groups = {}
 
-    pts2, seg1, seg2, nb_comparaison = [], [], [], 0
+    pts2, seg1, seg2, nb_comparaison, cmp = [], [], [], 0, [0,0]
 
     for i, point in enumerate(points):
         nb_comparaison += 1
-        if i not in register.keys():
+        if i not in groups.keys():
+            cmp[0] += 1
             x, y = point.coordinates
-
-            # Search last efficient index
-            print(i, last_efficient_x)
-            while last_efficient_x < len(points) and points[last_efficient_x].coordinates[0] <= x - distance:
-                nb_comparaison += 1
-                last_efficient_x += 1
 
             # Create new group
             groups[i] = set([i])
-            register[i] = i
 
-            j = last_efficient_x
-            while j < len(points) and points[j].coordinates[0] <= x + distance:
-                nb_comparaison += 1
-                aside_x, aside_y = points[j].coordinates
-
-                nb_comparaison += 1
-                if y - distance <= aside_y <= y + distance and point.distance_to(points[j]) <= distance:
+            for direction in [-1,1]:
+                j = i + direction
+                while 0 <= j < len(points) and x - distance <= points[j].coordinates[0] <= x + distance:
                     nb_comparaison += 1
-                    if j in register.keys() and i not in groups[register[j]]:
-                        aside_group_id = register[j]
-                        aside_group = groups[aside_group_id]
+                    
+                    nb_comparaison += 1
+                    if y - distance <= points[j].coordinates[1] <= y + distance and point.distance_to(points[j]) <= distance:
+                        nb_comparaison += 1
+                        if j in groups.keys() and i not in groups[j]:
+                            groups[i].update(groups[j])
 
-                        current_group_id = register[i]
+                            current_group_ids = groups[j]
+                            for k in current_group_ids:
+                                groups[k] = groups[i]
+                        else:
+                            groups[j] = groups[i]
+                            groups[i].add(j)
 
-                        # Réasignation des groupes
-                        for point_id in groups[current_group_id]:
-                            register[point_id] = aside_group_id
-                            aside_group.add(point_id)
+                        seg1.append(Segment([point, points[j]]))
+        
+                    seg2.append(Segment([point, points[j]]))
+                    cmp[1] += 1
+                    j += direction
 
-                        del groups[current_group_id]
-
-                    register[j] = register[i]
-                    groups[register[i]].add(j)
-
-                    seg1.append(Segment([point, points[j]]))
-    
-                seg2.append(Segment([point, points[j]]))
-                j += 1
-
-            pts2.append(point)
-            cercle = [Point([distance * cos(c*pi/10), distance * sin(c*pi/10)]) + point for c in range(20)]
-            seg2.append((Segment([p1, p2]) for p1, p2 in zip(cercle, islice(cycle(cercle), 1, None))))
-
+                pts2.append(point)
+                cercle = [Point([distance * cos(c*pi/10), distance * sin(c*pi/10)]) + point for c in range(20)]
+                seg2.append((Segment([p1, p2]) for p1, p2 in zip(cercle, islice(cycle(cercle), 1, None))))
 
 
     # Display
     print('Comparaison de point :', nb_comparaison)
-    tycat(pts2, *seg2)
+    tycat(points, *seg2)
 
     # Display
     tycat(points, *seg1)
 
-    result = list(len(group) for group in groups.values())
+    result = list((len(group) for group in (set((frozenset(group) for group in groups.values())))))
     result.sort(reverse=True)
 
     print(result)
+
+    end = perf_counter()
+    print(f"Performance : {end - start:.5f}")
+    print("Nombre de points :",len(points))
+    print("Comparaison / point :", cmp[1] / cmp[0])
 
 
 def main():
