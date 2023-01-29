99942d7dfd5fb8ef5a177a9b582af4e291dcd199
Beaufils Tao - Fix system <beaufilstao@gmail.com>
2023-01-29 13:07:49 +0100

    Add grouped && isolated

--- connectes.py
+++ connectes.py
@@ -36,16 +36,43 @@ def print_components_sizes(distance, points):
     points.sort()
 
     groups, result_ids = {}, set()
+    grouped, isolated = [], [0]
 
     for i, point in enumerate(points):
 
         if i not in groups.keys():
             x, y = point.coordinates
 
-            # Create new group
-            groups[i] = set([i])
+            # Remove inefficient
+            while len(grouped) > 0 and points[grouped[0]].coordinates[0] < x - distance:
+                nb_comparaison += 1
+                del grouped[0]
+            while len(isolated) > 0 and points[isolated[0]].coordinates[0] <= x:
+                nb_comparaison += 1
+                del isolated[0]
+
+            # Look at grouped
+            groups[i] = set([])
+
+            for point_id in grouped:
+                cmp[1] += 1
+                nb_comparaison += 1
+                if point.distance_to(points[point_id]) <= distance:
+                    groups[i].update(groups[point_id])
+                    seg1.append(Segment([point, points[point_id]]))
+
+                seg2.append(Segment([point, points[point_id]]))
+
+            for k in groups[i]:
+                groups[k] = groups[i]
+
+                if k in result_ids:
+                    result_ids.remove(k)
+
+            groups[i].add(i)
             result_ids.add(i)
 
+<<<<<<< HEAD
             for direction in [-1,1]:
                 j = i + direction
                 while 0 <= j < len(points) and x - distance <= points[j].coordinates[0] <= x + distance:
@@ -54,19 +81,61 @@ def print_components_sizes(distance, points):
 
                         if j in groups.keys() and i not in groups[j]:
                             groups[i].update(groups[j])
+=======
+            # Look at asolated
+            j = 0
+            while j < len(isolated):
+                nb_comparaison += 1
+>>>>>>> 54602b4 (Add grouped && isolated)
 
-                            current_group_ids = groups[j]
-                            for k in current_group_ids:
-                                groups[k] = groups[i]
+                seg2.append(Segment([point, points[isolated[j]]]))
+                cmp[1] += 1
 
-                                if k in result_ids:
-                                    result_ids.remove(k)
-                        else:
-                            groups[j] = groups[i]
-                            groups[i].add(j)
+                nb_comparaison += 1
+                if point.distance_to(points[isolated[j]]) <= distance:
+                    groups[isolated[j]] = groups[i]
+                    groups[i].add(isolated[j])
 
+<<<<<<< HEAD
                     j += direction
 
+=======
+                    seg1.append(Segment([point, points[isolated[j]]]))
+
+                    grouped.append(isolated[j])
+                    del isolated[j]
+                else:
+                    j += 1
+
+            # Look at new
+            j = isolated[-1] + 1 if len(isolated) > 0 else i + 1
+            while j < len(points) and points[j].coordinates[0] <= x + distance:
+                nb_comparaison += 1
+
+                cmp[1] += 1
+                nb_comparaison += 1
+                if point.distance_to(points[j]) <= distance:
+                    groups[j] = groups[i]
+                    groups[i].add(j)
+
+                    seg1.append(Segment([point, points[j]]))
+
+                    grouped.append(j)
+                else:
+                    isolated.append(j)
+
+                seg2.append(Segment([point, points[j]]))
+                j += 1
+
+            pts2.append(point)
+            cercle = [Point([distance * cos(c*pi/10), distance * sin(c*pi/10)]) + point for c in range(20)]
+            seg2.append((Segment([p1, p2]) for p1, p2 in zip(cercle, islice(cycle(cercle), 1, None))))
+
+
+    # Display
+    tycat(points, *seg2)
+    tycat(points, *seg1)
+>>>>>>> 54602b4 (Add grouped && isolated)
 
     result = list((len(groups[group_id]) for group_id in result_ids))
     result.sort(reverse=True)
@@ -75,6 +144,12 @@ def print_components_sizes(distance, points):
 
     end = perf_counter()
     print(f"Performance : {end - start:.5f}")
+<<<<<<< HEAD
+=======
+    print("Nombre de points :",len(points))
+    print('Comparaison de point :', nb_comparaison)
+    print("Comparaison / point :", cmp[1] / cmp[0])
+>>>>>>> 54602b4 (Add grouped && isolated)
 
 
 def main():
