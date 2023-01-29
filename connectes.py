8a1769baaec30ee19af208b3013d045fa5f5a65a
Beaufils Tao - Fix system <beaufilstao@gmail.com>
2023-01-29 22:09:48 +0100

    Remove tests

--- connectes.py
+++ connectes.py
@@ -34,17 +34,9 @@ def print_components_sizes(distance, points):
     points.sort()
 
     groups, result_ids = {}, set()
-    grouped, isolated = [], []
-    register = {}
+    grouped, isolated = [], [0]
 
     for i, point in enumerate(points):
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-        nb_comparaison += 1
->>>>>>> edc780c (Update isolated minimum)
-=======
->>>>>>> 49b8496 (Remove test)
 
         if i not in groups.keys():
             x, y = point.coordinates
@@ -59,7 +51,8 @@ def print_components_sizes(distance, points):
             groups[i] = set([])
 
             for point_id in grouped:
-                if point_id not in groups[i] and y - distance <= points[point_id].coordinates[1] <= y + distance and point.distance_to(points[point_id]) <= distance:
+
+                if point.distance_to(points[point_id]) <= distance:
                     groups[i].update(groups[point_id])
 
             for k in groups[i]:
@@ -71,57 +64,26 @@ def print_components_sizes(distance, points):
             groups[i].add(i)
             result_ids.add(i)
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-            for direction in [-1,1]:
-                j = i + direction
-                while 0 <= j < len(points) and x - distance <= points[j].coordinates[0] <= x + distance:
-
-                    if y - distance <= points[j].coordinates[1] <= y + distance and point.distance_to(points[j]) <= distance:
-
-                        if j in groups.keys() and i not in groups[j]:
-                            groups[i].update(groups[j])
-=======
             # Look at asolated
-=======
-            # Look at isolated
->>>>>>> edc780c (Update isolated minimum)
             j = 0
             while j < len(isolated):
-<<<<<<< HEAD
-                nb_comparaison += 1
->>>>>>> 54602b4 (Add grouped && isolated)
-=======
->>>>>>> 49b8496 (Remove test)
 
-                if y - distance <= points[isolated[j]].coordinates[1] <= y + distance and point.distance_to(points[isolated[j]]) <= distance:
+                if point.distance_to(points[isolated[j]]) <= distance:
                     groups[isolated[j]] = groups[i]
-
                     groups[i].add(isolated[j])
-                    register[isolated[j]] = i
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-                    j += direction
-
-=======
-                    seg1.append(Segment([point, points[isolated[j]]]))
-
-=======
->>>>>>> 49b8496 (Remove test)
                     grouped.append(isolated[j])
                     del isolated[j]
                 else:
                     j += 1
 
             # Look at new
-            j = isolated[-1] if len(isolated) > 0 else i + 1
+            j = isolated[-1] + 1 if len(isolated) > 0 else i + 1
             while j < len(points) and points[j].coordinates[0] <= x + distance:
 
-                if y - distance <= points[j].coordinates[1] <= y + distance and point.distance_to(points[j]) <= distance:
+                if point.distance_to(points[j]) <= distance:
                     groups[j] = groups[i]
                     groups[i].add(j)
-                    register[j] = i
 
                     grouped.append(j)
                 else:
@@ -129,38 +91,11 @@ def print_components_sizes(distance, points):
 
                 j += 1
 
-<<<<<<< HEAD
-            pts2.append(point)
-            cercle = [Point([distance * cos(c*pi/10), distance * sin(c*pi/10)]) + point for c in range(20)]
-            seg2.append((Segment([p1, p2]) for p1, p2 in zip(cercle, islice(cycle(cercle), 1, None))))
-            #seg1.append((Segment([p1, p2]) for p1, p2 in zip(cercle, islice(cycle(cercle), 1, None))))
-
-
-    # Display
-    tycat(pts2, *seg2)
-    tycat(points, *seg1)
->>>>>>> 54602b4 (Add grouped && isolated)
-
-=======
->>>>>>> 49b8496 (Remove test)
     result = list((len(groups[group_id]) for group_id in result_ids))
     result.sort(reverse=True)
 
     print(result)
 
-<<<<<<< HEAD
-    end = perf_counter()
-    print(f"Performance : {end - start:.5f}")
-<<<<<<< HEAD
-=======
-    print("Nombre de points :",len(points))
-    print('Nombre de if :', nb_comparaison)
-    print("Nombre d'analyse :", cmp[0])
-    print("Comparaison / point :", cmp[1] / cmp[0])
->>>>>>> 54602b4 (Add grouped && isolated)
-
-=======
->>>>>>> 49b8496 (Remove test)
 
 def main():
     """
