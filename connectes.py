ffdf4ac868760a5ee36c76d4a0a00e524f16fbb1
Beaufils Tao - Fix system <beaufilstao@gmail.com>
2023-03-10 22:52:41 +0100

    Update fusions && update main loop

--- connectes.py
+++ connectes.py
@@ -116,125 +116,99 @@ def print_components_sizes(distance, points):
     affichage des tailles triees de chaque composante
     """
 
-    with Perf("Global", True):
+    # x------------------x
+    # |  Initialization  |
+    # x------------------x
+
+    points.sort()
+
+    # Buckets
+
+    # Un "bucket" est une liste des points sur un découpage de l'axe Y.
+    # L'intersection entre "buckets" et vide.
+    
+    buckets = defaultdict(list)
+    buckets_limits, buckets_limits_others = defaultdict(int), defaultdict(int)
+
+    graph = (points, buckets, distance)
+
+    # A voir
+    for bucket_id, bucket_content in groupby(range(len(points)), lambda point_id: int(points[point_id][1] // distance)):
+        buckets[bucket_id].extend(bucket_content)
+
+    # Groups
+
+    # Le registre est la liste des correspondances (point, point référent).
+    # groups est l'état partiel courant des groupes et results en est l'état final.
+
+    register, fusions = {}, defaultdict(set)
+    wait_to_add = defaultdict(set)
+
+
+    # x-------------x
+    # |  Main loop  |
+    # x-------------x
+
+    for referent_id in filter(lambda point_id: point_id not in register, range(len(points))):
+
+        point = points[referent_id]
+
         # x------------------x
         # |  Initialization  |
         # x------------------x
 
-        with Perf("Global init"):
-            points.sort()
+        # Components
+        x, y = point
+        bucket_id = int(y // distance)
 
-            # Buckets
-
-            # Un "bucket" est une liste des points sur un découpage de l'axe Y.
-            # L'intersection entre "buckets" et vide.
-            
-            buckets = defaultdict(list)
-            buckets_limits, buckets_limits_others = defaultdict(int), defaultdict(int)
-
-            graph = (points, buckets, distance)
-
-            # A voir
-            for bucket_id, bucket_content in groupby(range(len(points)), lambda point_id: int(points[point_id][1] // distance)):
-                buckets[bucket_id].extend(bucket_content)
-
-            # Groups
-
-            # Le registre est la liste des correspondances (point, point référent).
-            # groups est l'état partiel courant des groupes et results en est l'état final.
-
-            register, fusions = {}, defaultdict(set)
-            wait_to_add = defaultdict(set)
-
-        # Tests
-        segments = { 'groups': [], 'fusions': [] }
-
-        # x-------------x
-        # |  Main loop  |
-        # x-------------x
-
-        for referent_id in filter(lambda point_id: point_id not in register, range(len(points))):
-
-            point = points[referent_id]
-
-            with Perf("Loop init"):
-                # x------------------x
-                # |  Initialization  |
-                # x------------------x
-
-                # Components
-                x, y = point
-                bucket_id = int(y // distance)
-
-                # Groups
-                register_add(register, referent_id, referent_id, wait_to_add, fusions)
+        # Groups
+        register_add(register, referent_id, referent_id, wait_to_add, fusions)
 
 
-            # x----------x
-            # |  Groups  |
-            # x----------x
+        # x----------x
+        # |  Groups  |
+        # x----------x
 
-            with Perf("Loop"):
-                # Observation des buckets à une distance de plus ou moins 2 du bucket courant
-                for near_id in filter(
-                        lambda near_id: register[near_id] != referent_id and referent_id not in fusions[register[near_id]], 
-                        iter_shift(graph, buckets_limits, bucket_id, (-1, 1), referent_id, False)
+        # Observation des buckets à une distance de plus ou moins 2 du bucket courant
+        for near_id in filter(
+                lambda near_id: register[near_id] != referent_id and referent_id not in fusions[register[near_id]], 
+                iter_shift(graph, buckets_limits, bucket_id, (-1, 1), referent_id, False)
+            ):
+            fusions[referent_id].add(register[near_id])
+
+        for near_id in iter_shift(graph, buckets_limits, bucket_id, (-1, 1), referent_id, True):
+
+            if near_id not in register.keys():
+
+                register_add(register, referent_id, near_id, wait_to_add, fusions)
+
+                for far_id in filter(
+                        lambda far_id: 
+                            far_id not in register or (
+                                register[far_id] != referent_id and
+                                ( register[far_id] not in fusions or referent_id      not in fusions[register[far_id]] ) and
+                                ( referent_id      not in fusions or register[far_id] not in fusions[referent_id     ] )
+                            ) and
+                            not is_at_distance(point, points[far_id], distance),
+                        iter_shift(graph, buckets_limits_others, bucket_id, (-1, +1), near_id)
                     ):
-                    fusions[referent_id].add(register[near_id])
 
-                for near_id in iter_shift(graph, buckets_limits, bucket_id, (-1, 1), referent_id, True):
-
-                    if near_id not in register.keys():
-
-                        register_add(register, referent_id, near_id, wait_to_add, fusions)
-
-                        segments['groups'].append((points[referent_id], points[near_id]))
-
-                        for far_id in filter(
-                                lambda far_id: 
-                                    far_id not in register or (
-                                        register[far_id] != referent_id and
-                                        ( register[far_id] not in fusions or referent_id      not in fusions[register[far_id]] ) and
-                                        ( referent_id      not in fusions or register[far_id] not in fusions[referent_id     ] )
-                                    ) and
-                                    not is_at_distance(point, points[far_id], distance),
-                                iter_shift(graph, buckets_limits_others, bucket_id, (-1, +1), near_id)
-                            ):
-
-                            if far_id in register.keys():
-                                fusions[referent_id].add(register[far_id])
-                            else:
-                                wait_to_add[far_id].add(referent_id)
-                            
-                    elif register[near_id] != referent_id and referent_id not in fusions[register[near_id]]:
-                        fusions[referent_id].add(register[near_id])
+                    if far_id in register.keys():
+                        fusions[referent_id].add(register[far_id])
+                    else:
+                        wait_to_add[far_id].add(referent_id)
+                    
+            elif register[near_id] != referent_id and referent_id not in fusions[register[near_id]]:
+                fusions[referent_id].add(register[near_id])
 
 
-        print(f"\n  Fin de la boucle principale : {Perf.times['Loop'][0]:7.4f}s\n")
+    # x-----------x
+    # |  Fusions  |
+    # x-----------x
 
+    # Calcul du résultat
+    counts = fusion(register, fusions)
 
-        # x-----------x
-        # |  Fusions  |
-        # x-----------x
-
-        # Tests
-        for point_id, nears in fusions.items():
-            for near_id in nears:
-                segments['fusions'].append((points[point_id], points[near_id]))
-        # -----
-
-
-        # Calcul du résultat
-        with Perf("Fusions"):
-            counts = fusion(register, fusions)
-
-
-    print(sorted(counts, reverse=True))
-    groups = defaultdict(set)
-    for near_id, referent_id in register.items():
-        groups[referent_id].add(near_id)
-
-    test_rapport(points, buckets, distance, groups, segments)
     print(sorted(counts, reverse=True))
 
     return sorted(counts, reverse=True) # To check validity
