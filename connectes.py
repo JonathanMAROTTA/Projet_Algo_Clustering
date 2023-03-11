b49c7ea940412f1cf0c383ce2df560b21c67ce75
Beaufils Tao - Fix system <beaufilstao@gmail.com>
2023-03-11 01:42:08 +0100

    Update comments

--- connectes.py
+++ connectes.py
@@ -4,10 +4,9 @@ compute sizes of all connected components.
 sort and display.
 """
 
-from math import floor
 from sys import argv
-from itertools import product, groupby
-from collections import defaultdict, Counter
+from itertools import groupby
+from collections import defaultdict
 
 from libtests import Perf, test_rapport
 
@@ -35,11 +34,15 @@ def load_instance(filename):
 
 def is_at_distance(point_1, point_2, distance):
     """
-        Def: Retourne le booléen indiquant si la distance en dimension 2
-             entre les deux points est inférieure ou égale à la distance donnée.
-        Pre-conditions:
-        - point_1 et point_2 sont des coordonnées de points
-        - distance est un réel positif.
+        Définition: Retourne le booléen indiquant si
+        la distance en dimension 2 entre les deux 
+        points est inférieure ou égale à la distance donnée.
+
+        Paramètres:
+        - point_1, point_2 : les deux coordonées de points
+        - distance : réel positif représentant une distance
+
+        Pré-conditions  : ...
         Post-conditions : ...
     """
 
@@ -47,65 +50,165 @@ def is_at_distance(point_1, point_2, distance):
     return dx*dx + dy*dy <= distance * distance
 
 
-# x-----------x
-# |  Buckets  |
-# x-----------x
+# x--------x
+# |  cuts  |
+# x--------x
 
-def iter_near(graph, limits, bucket_id, point_id, forward = None):
+def iter_near(graph, limits, cuts_key, point_id, forward = None):
+    """
+        Itérateur efficace sur les points à une distance
+        maximale d'un point et d'un cut donnée.
+        
+        Paramètres:
+        - graph : raccourci des structures données (points, cuts, distance)
+        - limits : dictionnaire d'entier (valeur par défaut 0) ayant pour clef
+                   un indice du dictionnaire de cut et pour valeur un indice
+                   de cut.
+        - cuts_key : clef du dictionnaire (cut à cibler)
+        - point_id : indice de point centre, indice non-itéré
+        - forward : booléen
+            * True  : itérer sur les points d'abscisses supérieures au point d'indice point_id
+            * False : itérer sur les points d'abscisses inférieures au point d'indice point_id
+            * None  : itérer sur tous les points
+
+        Pré-conditions  :
+        - l'indice point_id est supérieur à l'indice point_id de l'appel précédant 
+          (pour un même indice du dictionnaire de cut)
+        - les cuts sont triés 
+        - les valeurs de limits sont telle que tous indices du cut (de clef de dictionnaire
+          de cuts) inférieurs à la valeur ne peuvent être à bonne distance
+          
+        Post-conditions :
+        - les valeurs de limits sont telle que tous indices du cut (de clef de dictionnaire
+          de cuts) inférieurs à la valeur ne peuvent être à bonne distance
+    """
 
     # Initialisations
-    points, buckets, distance = graph
+    points, cuts, distance = graph
 
-    bucket = buckets[bucket_id]
+    cut = cuts[cuts_key]
     point = points[point_id]
 
     # -- Before limit --
 
-    bucket_in_id = limits[bucket_id]
+    cut_id = limits[cuts_key]
 
-    while bucket_in_id < len(bucket) and points[bucket[bucket_in_id]][0] < point[0] - distance:
-        bucket_in_id += 1
+    while cut_id < len(cut) and points[cut[cut_id]][0] < point[0] - distance:
+        cut_id += 1
 
-    limits[bucket_id] = bucket_in_id
+    limits[cuts_key] = cut_id
 
-    # -- Suivants --
+    # -- After limit --
 
-    while bucket_in_id < len(bucket) and bucket[bucket_in_id] < point_id:
+    if not forward or forward is None:
+        # Before
+        while cut_id < len(cut) and cut[cut_id] < point_id:
         
-        if (not forward or forward is None) and is_at_distance(point, points[bucket[bucket_in_id]], distance):
-            yield bucket[bucket_in_id]
+            if is_at_distance(point, points[cut[cut_id]], distance):
+                yield cut[cut_id]
 
-        bucket_in_id += 1
+            cut_id += 1
+    else:
+        # Skit before
+        while cut_id < len(cut) and cut[cut_id] < point_id:
+            cut_id += 1
 
-    bucket_in_id += bucket_in_id < len(bucket) and int(bucket[bucket_in_id] == point_id)
+    # Skip current
+    cut_id += cut_id < len(cut) and int(cut[cut_id] == point_id)
 
+    # After
     if forward or forward is None:
-        while bucket_in_id < len(bucket) and points[bucket[bucket_in_id]][0] <= point[0] + distance:
+        while cut_id < len(cut) and points[cut[cut_id]][0] <= point[0] + distance:
             
-            if is_at_distance(point, points[bucket[bucket_in_id]], distance):
-                yield bucket[bucket_in_id]
+            if is_at_distance(point, points[cut[cut_id]], distance):
+                yield cut[cut_id]
 
-            bucket_in_id += 1
+            cut_id += 1
 
-def iter_shift(graph, buckets_limits, bucket_id, relative_interval, point_id, forward = None):
+def iter_shift(graph, cuts_limits, cuts_key, relative_interval, point_id, forward = None):
+    """
+        Itérateur efficace sur les points à une distance
+        maximale d'un point et d'un interval de cuts donné.
+        
+        Paramètres:
+        - graph : raccourci des structures données (points, cuts, distance)
+        - limits : dictionnaire d'entier (valeur par défaut 0) ayant pour clef
+                   un indice du dictionnaire de cut et pour valeur un indice
+                   de cut.
+        - relative_interval : interval de clefs du dictionnaire (cuts à cibler)
+        - point_id : indice de point centre, indice non-itéré
+        - forward : booléen
+            * True  : itérer sur les points d'abscisses supérieures au point d'indice point_id
+            * False : itérer sur les points d'abscisses inférieures au point d'indice point_id
+            * None  : itérer sur tous les points
 
-    for aside_bucket_id in range(max(bucket_id + relative_interval[0], 0), bucket_id + relative_interval[1] + 1):
-        yield from iter_near(graph, buckets_limits, aside_bucket_id, point_id, forward)
+        Pré-conditions  :
+        - l'indice point_id est supérieur à l'indice point_id de l'appel précédant 
+          (pour un même indice du dictionnaire de cut)
+        - les cuts sont triés 
+        - les valeurs de limits sont telle que tous indices du cut (de clef de dictionnaire
+          de cuts) inférieurs à la valeur ne peuvent être à bonne distance
+          
+        Post-conditions :
+        - les valeurs de limits sont telle que tous indices du cut (de clef de dictionnaire
+          de cuts) inférieurs à la valeur ne peuvent être à bonne distance
+    """
+
+    for aside_cut_id in range(max(cuts_key + relative_interval[0], 0), cuts_key + relative_interval[1] + 1):
+        yield from iter_near(graph, cuts_limits, aside_cut_id, point_id, forward)
 
 
 # x------------x
-# |  Register  |
+# |  referents  |
 # x------------x
 
-def register_add(register, referent_id, near_id, wait_to_add, fusions):
-    register[near_id] = referent_id
+def not_already_merged(referents, cluster_to_merge, referent_id, point_id):
+    """
+        Prédicat indiquant si deux points ont été fusionnés.
+        
+        Paramètres:
+        - referents : dictionnaire des référents
+        - cluster_to_merge : cluster à fusionner
+        - referent_id : point 1 (référent)
+        - point_id : point 2
+
+        Pré-conditions  : ...
+        Post-conditions : ...
+    """
+
+    return point_id not in referents or (
+        referents[point_id] != referent_id and
+        ( referents[point_id] not in cluster_to_merge or referent_id         not in cluster_to_merge[referents[point_id]] ) and
+        ( referent_id         not in cluster_to_merge or referents[point_id] not in cluster_to_merge[referent_id        ] )
+    )
+
+def referents_add(referents, cluster_to_merge, wait_to_merge, referent_id, point_id):
+    """
+        Ajoute un référent à un point.
+        
+        Paramètres:
+        - referents : dictionnaire des référents
+        - cluster_to_merge : cluster à fusionner
+        - wait_to_merge : liste des points en attente de fusion
+        - referent_id : point 1 (référent)
+        - point_id : point 2
+
+        Pré-conditions  :
+        - point_id ne possède pas de référent
+        Post-conditions : ...
+    """
+
+    # Ajout du référent dans le dictionnaire de référents
+    referents[point_id] = referent_id
+
+    # Ajout d'une fusion pour chaque fusion en attente pour le point point_id
     for old_ref_id in filter(
-            lambda old_ref_id : referent_id != old_ref_id and ( referent_id not in fusions or old_ref_id not in fusions[referent_id] ), 
-            wait_to_add[near_id]
+            lambda old_ref_id : referent_id != old_ref_id and ( referent_id not in cluster_to_merge or old_ref_id not in cluster_to_merge[referent_id] ), 
+            wait_to_merge[point_id]
         ):
-        fusions[old_ref_id].add(referent_id)
+        cluster_to_merge[old_ref_id].add(referent_id)
 
-    wait_to_add[near_id].clear()
+    wait_to_merge[point_id].clear()
 
 # x--------x
 # |  Main  |
@@ -124,36 +227,33 @@ def print_components_sizes(distance, points):
         with Perf("Global init"):
             points.sort()
 
-            # Buckets
+            # Cuts
+            cuts = defaultdict(list)
 
-            # Un "bucket" est une liste des points sur un découpage de l'axe Y.
-            # L'intersection entre "buckets" et vide.
-            
-            buckets = defaultdict(list)
-            buckets_limits, buckets_limits_others = defaultdict(int), defaultdict(int)
+            for cut_key, cut_content in groupby(range(len(points)), lambda point_id: int(points[point_id][1] // distance)):
+                cuts[cut_key].extend(cut_content)
 
-            graph = (points, buckets, distance)
+            # Limits
+            cuts_limits, cuts_limits_others = defaultdict(int), defaultdict(int)
 
-            # A voir
-            for bucket_id, bucket_content in groupby(range(len(points)), lambda point_id: int(points[point_id][1] // distance)):
-                buckets[bucket_id].extend(bucket_content)
+            # Shortcut
+            graph = (points, cuts, distance)
 
-            # Groups
+            # Clustering
+            referents = {}
 
-            # Le registre est la liste des correspondances (point, point référent).
-            # groups est l'état partiel courant des groupes et results en est l'état final.
-
-            register, fusions = {}, defaultdict(set)
-            wait_to_add = defaultdict(set)
+            cluster_to_merge = defaultdict(set)
+            wait_to_merge    = defaultdict(set)
 
         # Tests
-        segments = { 'groups': [], 'fusions': [] }
+        segments = { 'groups': [], 'cluster_to_merge': [] }
 
         # x-------------x
         # |  Main loop  |
         # x-------------x
 
-        for referent_id in filter(lambda point_id: point_id not in register, range(len(points))):
+        # Itération sur les points n'ayant pas de référents
+        for referent_id in filter(lambda point_id: point_id not in referents, range(len(points))):
 
             point = points[referent_id]
 
@@ -163,11 +263,11 @@ def print_components_sizes(distance, points):
                 # x------------------x
 
                 # Components
-                x, y = point
-                bucket_id = int(y // distance)
+                y = point[1]
+                cut_id = int(y // distance)
 
-                # Groups
-                register_add(register, referent_id, referent_id, wait_to_add, fusions)
+                # Le référent du point proche devient lui-même
+                referents_add(referents, cluster_to_merge, wait_to_merge, referent_id, referent_id)
 
 
             # x----------x
@@ -175,72 +275,88 @@ def print_components_sizes(distance, points):
             # x----------x
 
             with Perf("Loop"):
-                # Observation des buckets à une distance de plus ou moins 2 du bucket courant
+                # Itération sur les points :
+                # - de référent différent du point courant
+                # - non fusionné avec le cluster courant
+                # - d'abscisse inférieure au point courant
+                # - de bonne distance au point courant
+
                 for near_id in filter(
-                        lambda near_id: register[near_id] != referent_id and referent_id not in fusions[register[near_id]], 
-                        iter_shift(graph, buckets_limits, bucket_id, (-1, 1), referent_id, False)
+                        lambda near_id: referents[near_id] != referent_id and referent_id not in cluster_to_merge[referents[near_id]], 
+                        iter_shift(graph, cuts_limits, cut_id, (-1, 1), referent_id, False)
                     ):
-                    fusions[referent_id].add(register[near_id])
+                    cluster_to_merge[referent_id].add(referents[near_id])
 
-                for near_id in iter_shift(graph, buckets_limits, bucket_id, (-1, 1), referent_id, True):
+                # Itération sur les points :
+                # - d'abscisse supérieure au point courant
+                # - de bonne distance au point courant
 
-                    if near_id not in register.keys():
+                for near_id in iter_shift(graph, cuts_limits, cut_id, (-1, 1), referent_id, True):
 
-                        register_add(register, referent_id, near_id, wait_to_add, fusions)
+                    if near_id not in referents.keys():
+
+                        # Le référent du point proche devient le point courant
+                        referents_add(referents, cluster_to_merge, wait_to_merge, referent_id, near_id)
 
                         segments['groups'].append((points[referent_id], points[near_id]))
 
+                        # Itération sur les points :
+                        # - de bonne distance du point proche
+                        # - non à bonne distance du point courant
+                        # - non déjà fusionné avec le point courant
+
                         for far_id in filter(
-                                lambda far_id: 
-                                    far_id not in register or (
-                                        register[far_id] != referent_id and
-                                        ( register[far_id] not in fusions or referent_id      not in fusions[register[far_id]] ) and
-                                        ( referent_id      not in fusions or register[far_id] not in fusions[referent_id     ] )
-                                    ) and
+                                lambda far_id: not_already_merged(referents, cluster_to_merge, referent_id, far_id) and
                                     not is_at_distance(point, points[far_id], distance),
-                                iter_shift(graph, buckets_limits_others, bucket_id, (-1, +1), near_id)
+                                iter_shift(graph, cuts_limits_others, cut_id, (-1, +1), near_id)
                             ):
 
-                            if far_id in register.keys():
-                                fusions[referent_id].add(register[far_id])
+                            if far_id in referents.keys():
+
+                                # Ajout d'une fusion
+                                cluster_to_merge[referent_id].add(referents[far_id])
                             else:
-                                wait_to_add[far_id].add(referent_id)
+
+                                # Ajout d'une fusion future
+                                wait_to_merge[far_id].add(referent_id)
                             
-                    elif register[near_id] != referent_id and referent_id not in fusions[register[near_id]]:
-                        fusions[referent_id].add(register[near_id])
+                    elif referents[near_id] != referent_id and referent_id not in cluster_to_merge[referents[near_id]]:
+
+                        # Ajout d'une fusion
+                        cluster_to_merge[referent_id].add(referents[near_id])
 
 
         print(f"\n  Fin de la boucle principale : {Perf.times['Loop'][0]:7.4f}s\n")
 
 
         # x-----------x
-        # |  Fusions  |
+        # |  cluster_to_merge  |
         # x-----------x
 
         # Tests
-        for point_id, nears in fusions.items():
+        for point_id, nears in cluster_to_merge.items():
             for near_id in nears:
-                segments['fusions'].append((points[point_id], points[near_id]))
+                segments['cluster_to_merge'].append((points[point_id], points[near_id]))
         # -----
 
 
         # Calcul du résultat
-        with Perf("Fusions"):
-            counts = fusion(register, fusions)
+        with Perf("Fusion"):
+            counts = fusion(referents, cluster_to_merge)
 
 
     print(sorted(counts, reverse=True))
     groups = defaultdict(set)
-    for near_id, referent_id in register.items():
+    for near_id, referent_id in referents.items():
         groups[referent_id].add(near_id)
 
-    test_rapport(points, buckets, distance, groups, segments)
+    test_rapport(points, cuts, distance, groups, segments)
     print(sorted(counts, reverse=True))
 
     return sorted(counts, reverse=True) # To check validity
 
 
-def init_counts(register):
+def init_counts(referents):
     """
     Create a dict of :\n
     key = centroid\n
@@ -248,16 +364,16 @@ def init_counts(register):
     """
     counts = defaultdict(int)
 
-    for _, point_id in register.items():
+    for _, point_id in referents.items():
         counts[point_id] += 1
 
     return counts
     
 
-def fusion(register, fusions):
+def fusion(referents, cluster_to_merge):
     """Réalise la fusion des points dans des groupes définitifs"""
 
-    counts = init_counts(register)
+    counts = init_counts(referents)
 
     # c1 ----> c2 ----> c3 ----> c1
     # c1       c2       c3       c1
@@ -269,30 +385,30 @@ def fusion(register, fusions):
 
     # On peut voir fusion comme la gestion de différents arbres.
 
-    # fusions   ; key   : referent_id
+    # cluster_to_merge   ; key   : referent_id
     #           ; value : set(referent_id)
 
-    # fusions   ; key   : node_id
+    # cluster_to_merge   ; key   : node_id
     #           ; value : set(node_id)
 
-    for node, nodes_to_merge in fusions.items():
+    for node, nodes_to_merge in cluster_to_merge.items():
 
         # Recherche de la racine
-        root = register[node]
-        while root != register[root]:
-            root = register[root]
+        root = referents[node]
+        while root != referents[root]:
+            root = referents[root]
 
-        register[node] = root
+        referents[node] = root
 
 
         for node_to_merge in nodes_to_merge:
 
             # Recherche de la racine
-            root_to_merge = register[node_to_merge]
-            while root_to_merge != register[root_to_merge]:
-                root_to_merge = register[root_to_merge]
+            root_to_merge = referents[node_to_merge]
+            while root_to_merge != referents[root_to_merge]:
+                root_to_merge = referents[root_to_merge]
 
-            register[node_to_merge] = root_to_merge
+            referents[node_to_merge] = root_to_merge
 
 
             if root != root_to_merge:
@@ -301,7 +417,7 @@ def fusion(register, fusions):
                 counts[root] += counts[root_to_merge]
 
                 # Suppression de l'arbre d'identifiant root_to_merge
-                register[root_to_merge] = root
+                referents[root_to_merge] = root
                 del counts[root_to_merge]
 
     return list(number_of_child for number_of_child in counts.values())
